3cb600c53912a642e3313eca5c5e7fde1b723bbd
aseokana <aseokana@bhp.org.bw>
2018-01-21 10:10:04 +0200

    upload function

--- app/src/main/java/open/hive/barter/UploadActivity.java
+++ app/src/main/java/open/hive/barter/UploadActivity.java
@@ -1,13 +1,132 @@
 package open.hive.barter;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class UploadActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+
+public class UploadActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final int PICK_IMAGE_REQUEST = 24;
+
+    private ImageView imgFile;
+    private Button btnSelect, btnUpload;
+
+    private Uri filepath;
+
+    private StorageReference storageReference;
+    private FirebaseAuth firebaseAuth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
+
+        storageReference = FirebaseStorage.getInstance().getReference();
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if (firebaseAuth == null){
+            finish();
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+
+        imgFile = findViewById(R.id.imgFile);
+        btnSelect = findViewById(R.id.btnSelect);
+        btnUpload = findViewById(R.id.btnUpload);
+
+        btnSelect.setOnClickListener(this);
+        btnUpload.setOnClickListener(this);
+    }
+
+    private void showFileSelector(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"Select an Image"), PICK_IMAGE_REQUEST);
+    }
+
+    private void uploadFile(){
+
+        if (filepath != null){
+
+            Toast.makeText(this, "Uploading", Toast.LENGTH_LONG).show();
+
+            StorageReference riversRef = storageReference.child("images/"+filepath.getLastPathSegment());
+            riversRef.putFile(filepath)
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            // Handle unsuccessful uploads
+                            Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            // Handle successful uploads
+                            Toast.makeText(getApplicationContext(), "file uploaded", Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            //track the progress
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred())/taskSnapshot.getTotalByteCount();
+                            Toast.makeText(getApplicationContext(), ((int) progress)+"% Uploaded", Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+        }else{
+            //display an error toast
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultcode, Intent data){
+        super.onActivityResult(requestCode, resultcode,data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultcode == RESULT_OK & data != null && data.getData() != null){
+
+            filepath = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filepath);
+                imgFile.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v == btnSelect){
+            //open file selector
+            showFileSelector();
+        } else if (v == btnUpload){
+            //upload file to firebase
+            uploadFile();
+        }
+
     }
 }
