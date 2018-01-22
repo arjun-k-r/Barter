eb5d38ec9b7f1be111335c3c105bc6bdac18b98c
aseokana <aseokana@bhp.org.bw>
2018-01-22 08:31:42 +0200

    importing sweet-alert-dialog

--- app/src/main/java/open/hive/barter/ItemsUploadedActivity.java
+++ app/src/main/java/open/hive/barter/ItemsUploadedActivity.java
@@ -26,8 +26,9 @@ public class ItemsUploadedActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                startActivity(new Intent(getApplicationContext(), UploadActivity.class));
             }
         });
     }
